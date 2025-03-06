9eba3ac49bc2ffcddb599808af04c22304df1311
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-03-06 16:26:38 +0600

    try to emplement form data

--- src/Pages/AddService.jsx
+++ src/Pages/AddService.jsx
@@ -3,21 +3,38 @@ import React from 'react';
 const AddService = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(e.target)
+         const formData=new FormData(e.target)
+         const initialData =Object.fromEntries(formData.entries())
+         console.log(initialData)
+        // const form = e.target
+        // const imgURL = form.imgURL.value
+        // const serviceName = form.serviceName.value
+        // const price = form.price.value
+        // const serviceArea= form.serviceArea.value
+        // const description = form.description.value
+        // console.log(imgURL,serviceName,price,serviceArea,description)
     }
     return (
         <div className="shadow-2xl card bg-base-100">
-            <h1 className="pt-5 text-4xl font-bold text-center">Login now!</h1>
+            <h1 className="pt-5 text-4xl font-bold text-center">Add your service!</h1>
             <div className="card-body">
                 <form onSubmit={handleSubmit}>
                     <fieldset className="fieldset" >
-                        <label className="fieldset-label">Email : </label>
-                        <input type="email" className="w-full input-bordered input" placeholder="Email" />
-                        <label className="fieldset-label">Password : </label>
-                        <input type="password" className="w-full input input-bordered" placeholder="Password" />
+                        <label className="fieldset-label">ImageURL : </label>
+                        <input type="text" className="w-full input-bordered input" placeholder="ImageURL" name='imgURL'/>
+                        <label className="fieldset-label">Service Name : </label>
+                        <input type="text" className="w-full input input-bordered" placeholder="Service Name" name='serviceName'/>
+                        <label className="fieldset-label">Price : </label>
+                        <input type="text" className="w-full input-bordered input" placeholder="Price" name='price'/>
+                        
+                        <label className="fieldset-label">Service Area : </label>
+                        <input type="text" className="w-full input input-bordered" placeholder="Service Area" name='serviceArea'/>
+                        
+                        <label className="fieldset-label">Description : </label>
+                        <input type="text" className="w-full input input-bordered" placeholder="Description" name='description'/>
 
                     </fieldset>
-                    <button className="w-full mt-4 btn btn-neutral" type='submit'>Login</button>
+                    <button className="w-full mt-4 btn btn-neutral" type='submit'>Add service</button>
                 </form>
             </div>
         </div>
