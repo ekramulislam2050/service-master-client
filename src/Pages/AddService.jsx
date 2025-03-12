10b87f482787d18df53a75d76ad9bc3b397bd448
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-03-12 09:06:47 +0600

    json data upload complet

--- src/Pages/AddService.jsx
+++ src/Pages/AddService.jsx
@@ -39,9 +39,9 @@ const AddService = () => {
                     {/* service provider related------------------ */}
                     {
                         user && <fieldset className="mb-5 fieldset" >
-                            
+
                             <legend className='font-bold text-center'>Information of Provider</legend>
-                            
+
                             <label className="fieldset-label">Provider Name : </label>
                             <input type="text" className="w-full input input-bordered" placeholder="Provider Name" name='providerName' defaultValue={user.displayName} />
 
@@ -49,28 +49,28 @@ const AddService = () => {
                             <input type="email" className="w-full input-bordered input" placeholder="Provider Email" name='providerEmail' defaultValue={user.email} />
 
                             <label className="fieldset-label">Provider Image URL : </label>
-                            <input type="text" className="w-full input-bordered input" placeholder="Provider Image URL" name='providerImgURL' defaultValue={user.photoURL} />
+                            <input type="text" className="w-full input-bordered input" placeholder="Provider Image URL" name='providerImageURL' defaultValue={user.photoURL} />
 
                         </fieldset>
                     }
                     {/* service related----------------- */}
                     <fieldset className="fieldset" >
-                    <legend className='font-bold text-center '>Information of Service</legend>
-                    
+                        <legend className='font-bold text-center '>Information of Service</legend>
+
                         <label className="fieldset-label">Service Name : </label>
                         <input type="text" className="w-full input input-bordered" placeholder="Service Name" name='serviceName' />
 
-                        <label className="fieldset-label">LogoURL : </label>
-                        <input type="text" className="w-full input-bordered input" placeholder="logoURL" name='logoURL' />
+                        <label className="fieldset-label">Service Image URL : </label>
+                        <input type="text" className="w-full input-bordered input" placeholder="imgURL" name='serviceImageURL' />
 
                         <label className="fieldset-label">Price : </label>
-                        <input type="text" className="w-full input-bordered input" placeholder="Price" name='price' />
+                        <input type="text" className="w-full input-bordered input" placeholder="Price" name='servicePrice' />
 
                         <label className="fieldset-label">Service Area : </label>
                         <input type="text" className="w-full input input-bordered" placeholder="Service Area" name='serviceArea' />
 
                         <label className="fieldset-label">Service Description : </label>
-                        <textarea className="w-full input input-bordered" placeholder="Service Description" name='description' maxLength={100} />
+                        <textarea className="w-full input input-bordered" placeholder="Service Description" name='serviceDescription' maxLength={100} />
 
                     </fieldset>
                     <button className="w-full mt-4 btn btn-neutral" type='submit'>Add service</button>
